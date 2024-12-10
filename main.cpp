86164429f10160da695b32bdebe3284e4ed57926
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2024-12-09 21:48:39 -0800

    initial? commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "wiringPi.h"
+
+int main() {
+    wiringPiSetup();
+    std::cout << "Hello, world?!\n";
+    return 0;
+}
---
+++ toolchain.cmake
@@ -0,0 +1,16 @@
+# https://cmake.org/cmake/help/book/mastering-cmake/chapter/Cross%20Compiling%20With%20CMake.html
+
+# Make it cross-compile for aarch64
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+
+set(CMAKE_C_COMPILER aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER aarch64-linux-gnu-g++-12)
+
+# adjust the default behavior of the FIND_XXX() commands:
+# search programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+# search headers and libraries in the target environment
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
